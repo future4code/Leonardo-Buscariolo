67d6ba773f7df92aff12e0eea6d4404ca8c6d6a3
LeonardoSofiati <leonardo.sofiati17@gmail.com>
2021-07-15 00:43:40 -0300

    quinzena1-aula03

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,78 @@
+/* Exercicios de interpretação de código
+Exercicio 1
+Resposta: será impresso 10, 10 e 5, nessa ordem
+
+Exercicio 2
+Resposta: será impresso 10, 10 e 10, nessa odem
+
+Exercicio 1
+Resposta:
+let p = o melhor seria let cargaHoraria
+let t = o melhor seria let salarioDia
+
+O programa irá pegar o seu salario por dia, dividir pela quantidade de horas trabalhadas por dia, para resultar o seu salário por hora.
+*/
+
+// Exercicios de escrita de código
+/*Exercicio 1
+
+let nome;
+let idade;
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// Resultado: foi impresso "undefined" pois isso é uma variavel vazia não intencional, a qual ainda não foi preenchida pois foi esquecida ou ainda será definida
+
+nome = prompt("Qual seu nome?");
+idade = prompt("Qual sua idade?");
+
+console.log(nome);
+console.log(idade);
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// Resultado: retornaram duas strings, pois todo resultado de um prompt() retorna como string, mesmo que o input seja numérico.
+
+console.log("Olá", nome, "você tem", idade, "anos");
+*/
+
+/*Exercicio 2
+
+const bandaPreferida = prompt("Você gosta de Beatles?");
+const comidaPreferida = prompt("Você gosta de pastel?");
+const climaPreferido = prompt("Você gosta do frio?");
+
+console.log("Você gosta de Beatles? - ",bandaPreferida);
+console.log("Você gosta de pastel? - ",comidaPreferida);
+console.log("Você gosta do frio? - ",climaPreferido);
+*/
+
+/*Exercicio 3
+
+let a = 10;
+let b = 25;
+let c = a;
+
+a = b;
+b = c;
+
+console.log("O novo valor de a é", a);
+console.log("O novo valor de b é", b);
+*/
+
+//Desafio
+
+let primeiroNumero = Number(prompt("Digite um número de 1 a 10"));
+let segundoNumero = Number(prompt("Digite outro número de 1 a 10"));
+
+console.log("O primeiro número somado ao segundo número resulta em:", primeiroNumero + segundoNumero);
+console.log("O primeiro npumero multiplicado pelo segundo número resulta em:", primeiroNumero * segundoNumero);
+
+
+
+
+
+
+
