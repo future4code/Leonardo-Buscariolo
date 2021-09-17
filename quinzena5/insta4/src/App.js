16f98b301dadf82f82843a47938bee303c9e2966
LeonardoSofiati <leonardo.sofiati17@gmail.com>
2021-09-16 22:27:18 -0300

    quinzena5-aula20 insta4 versao2

--- quinzena5/insta4/src/App.js
+++ quinzena5/insta4/src/App.js
@@ -10,27 +10,96 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const InputArea = styled.div`
+  display: flex;
+
+`
+
+const InputFiel = styled.input`
+
+`
+const InputButton = styled.button`
+  
+`
+
 class App extends React.Component {
+  state = {
+    listaDeUsuario: [
+      {
+      nomeUsuario: "paulinha",
+      fotoUsuario: "https://picsum.photos/50/55",
+      fotoPost: "https://picsum.photos/200/150"
+      },
+
+      {
+        nomeUsuario: "leonardo",
+        fotoUsuario: "https://picsum.photos/50/51",
+        fotoPost: "https://picsum.photos/200/151"
+      },
+
+      {
+        nomeUsuario: "julia",
+        fotoUsuario: "https://picsum.photos/50/52",
+        fotoPost: "https://picsum.photos/200/152"
+      }
+    ],
+
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+  }
+
+    adicionarPost = () => {
+      const novoPost = {
+        nomeUsuario: this.state.valorInputNomeUsuario,
+        fotoUsuario: this.state.valorInputFotoUsuario,
+        fotoPost: this.state.valorInputFotoPost,
+      };
+
+      const novoPostInserido = [...this.state.listaDeUsuario, novoPost];
+
+      this.setState({listaDeUsuario: novoPostInserido});
+      this.setState({
+        valorInputNomeUsuario: "",
+        valorInputFotoUsuario: "",
+        valorInputFotoPost: ""
+      });
+    }
+
+    onChangeInputNomeUsuario = (event) => {
+      this.setState({valorInputNomeUsuario: event.target.value});
+    }
+
+    onChangeInputFotoUsuario = (event) => {
+      this.setState({valorInputFotoUsuario: event.target.value});
+    }
+
+    onChangeInputFotoPost = (event) => {
+      this.setState({valorInputFotoPost: event.target.value});
+    }
+
   render() {
+
+    const listaMapeadaUsuario = this.state.listaDeUsuario.map((pessoa) => {
+        return (
+          <Post
+          nomeUsuario={pessoa.nomeUsuario}
+          fotoUsuario={pessoa.fotoUsuario}
+          fotoPost={pessoa.fotoPost} 
+          />
+        )
+    })
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/55'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
+        <InputArea>
+          <InputFiel placeholder={'Nome do Usuario'} value={this.state.valorInputNomeUsuario} onChange={this.onChangeInputNomeUsuario}></InputFiel>
+          <InputFiel placeholder={'Foto do Usuario'} value={this.state.valorInputFotoUsuario} onChange={this.onChangeInputFotoUsuario}></InputFiel>
+          <InputFiel placeholder={'Foto do Post'} value={this.state.valorInputFotoPost} onChange={this.onChangeInputFotoPost}></InputFiel>
+          <InputButton onClick={this.adicionarPost}>Adicionar Post</InputButton>
+        </InputArea>
 
-        <Post
-          nomeUsuario={'leonardo'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
+        {listaMapeadaUsuario}
 
-        <Post
-          nomeUsuario={'julia'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
         <SecaoCompartilhar>
         </SecaoCompartilhar>
       </MainContainer>
--- quinzena5/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ quinzena5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -3,35 +3,68 @@ import styled from 'styled-components'
 
 const CommentContainer = styled.div`
     display: flex;
+	flex-direction: column;
     justify-content: center;
     padding: 5px;
 `
 
 const InputComentario = styled.input`
-    width: 100%;
-    margin-right: 5px;
+    width: 97%;
+	margin-bottom: 5px;
+`
+
+const ImagemUsuario = styled.img`
+    width: 90px;
+	height: 90px;
+	background-color: red;
 `
 
 export class SecaoComentario extends Component {
 	state = {
-		valorComentario: ''
+		comentario: [ 
+			{
+			valorComentario: ''
+			},
+		],
+	valorComentario: ''
+	}
+
+	adicionaComentario = () => {
+		const novoComentario = {
+			valorComentario: this.state.valorComentario
+		}
+
+		const novoComentarioAdicionado = [...this.state.comentario, novoComentario]
+
+		this.setState({comentario: novoComentarioAdicionado})
+		this.setState({valorComentario: ''})
 	}
 
 	onChangeComentario = (event) => {
 		this.setState({
 			valorComentario: event.target.value
 		})
-		console.log(event.target.value);
+		console.log(event.target.value)
 	}
 
 	render() {
+
+		const novoComentarioRenderizado = this.state.comentario.map((comentario) => {
+			return (
+				<p>
+					{comentario.valorComentario}
+				</p>
+			)
+		})
+
 		return <CommentContainer>
 			<InputComentario
 				placeholder={'Comentário'}
 				value={this.state.valorComentario}
 				onChange={this.onChangeComentario}
 			/>
-			<button onClick={this.props.aoEnviar}>Enviar</button>
+			<button onClick={this.props.aoEnviar} onClick={this.adicionaComentario}>Enviar</button>
+			{novoComentarioRenderizado}
 		</CommentContainer>
 	}
 }
